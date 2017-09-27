c870c7a499235e65b2aa5d5a22e3e525e8cbfdd1
Nikola Djukanovic <ndjukanovic@localhost.localdomain>
2017-09-27 22:19:55 +0200

    Added inflation functionality

--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-column',
-  templateUrl: './column.component.html',
-  styleUrls: ['./column.component.css']
+	selector: 'app-column',
+	templateUrl: './column.component.html',
+	styleUrls: ['./column.component.css']
 })
 export class ColumnComponent implements OnInit {
+	inflation: number = 0;
+	emptyDivPercentage: number = 20;
+	containerDivePercentage: number = 80;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	onInflationChange(value) {
+		this.inflation = this.setInflation(value);
+
+		this.emptyDivPercentage = 20 - (this.inflation / 5) * 20;
+		this.containerDivePercentage = 100 - this.emptyDivPercentage;
+		console.log(this.emptyDivPercentage);
+		console.log(this.containerDivePercentage);
+	}
+
+	setInflation(value): number {
+		if (value > 5) return 5;
+		if (value < 0) return 0;
+
+		return value as number;
+	}
 
 }
