fb84c8a3468c24df687f9a933f165a871ea82abb
Nikola Đukanović <nikola.djukanovic@impeodevelopment.rs>
2017-10-03 23:05:27 +0200

    Added logic for red segment

--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -14,6 +14,14 @@ export class ColumnComponent {
 	containerDivePercentage: number = 80;
 	minimalValue = 50;
 
+	totalRedPercentage: number = 40;
+	totalGreenPercentage: number = 20;
+	totalBluePercentage: number = 40;
+
+	blueColor: string = "blue";
+	greenColor: string = "green";
+	redColor: string = "red";
+
 	constructor() { }
 
 	// tslint:disable-next-line:use-life-cycle-interface
@@ -49,6 +57,52 @@ export class ColumnComponent {
 	getSum(array: number[]): number {
 		let sum = 0;
 		array.forEach(x => sum += x);
+
+		return sum;
+	}
+
+	getLinearGradient(column: ColumnItem, i: number): string {
+		console.log(column.label);
+		let sumOfPreviousPercentages = this.getSumOfPreviousPercentages(i);
+		console.log("sum: " + sumOfPreviousPercentages);
+
+		let redPercentage = this.getRedGradientPercentage(sumOfPreviousPercentages, i);
+		let greenPercentage = 100 - redPercentage;
+
+		let redParams = `${this.redColor} 0%, ${this.redColor} ${redPercentage}%`;
+		let greenParams = `${this.greenColor} ${redPercentage}%, ${this.greenColor} ${greenPercentage}%`;
+		let blueParams = `${this.blueColor} ${greenPercentage}%, ${this.blueColor} 100%`;
+		let gradientString = `linear-gradient(to bottom,${redParams}, ${greenParams}, ${blueParams})`;
+
+		console.log(gradientString);
+
+		return gradientString;
+	}
+
+	/**
+	 * TODO: Refactor
+	 */
+	getRedGradientPercentage(sumOfPrevious: number, index) {
+		if (sumOfPrevious < this.totalRedPercentage) return 100;
+
+		console.log("%: " + this.getColumnHeightPercentage(this.columnItems[index].value));
+		console.log("dif: " + (sumOfPrevious - this.totalRedPercentage));
+
+		let result = (this.totalRedPercentage - (sumOfPrevious - this.getColumnHeightPercentage(this.columnItems[index].value)))
+			/ this.getColumnHeightPercentage(this.columnItems[index].value) * 100;
+
+		if (result < 0) return 0;
+
+		return result;
+	}
+
+	getSumOfPreviousPercentages(index: number): number {
+		let sum = 0;
+
+		for (let i = 0; i <= index; i++) {
+			let columnValue = this.columnItems[i].value;
+			sum += this.getColumnHeightPercentage(columnValue);
+		}
 
 		return sum;
 	}
